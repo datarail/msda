39f1bb8c3aa4542f55e601bcf3e8905cb9248cdf
subkar <sub.kartik@gmail.com>
2016-08-02 08:00:19 -0400

    added functions to get apoptotoc and membrane protein subsets

--- msda/subset.py
+++ msda/subset.py
@@ -7,10 +7,14 @@ import re
 
 def get_kinase_set(df):
 
+    # From phosphosite plus
+    # =====================
     # df_ks = pd.read_csv('resources/Kinase_Substrate_Dataset.csv',
     #                     sep='\t', header=2)
     # kinase_list = list(set(df_ks.KIN_ACC_ID.tolist()))
 
+    # From Kinome website 2007 paper
+    # ==============================
     # df_kin = pd.read_excel('resources/Kincat_Hsap.xls')
     # df_s = df_kin[df_kin['Pseudogene?'] == 'N']
     # entrez_list = df_s['Entrez_GENEID']
@@ -62,6 +66,23 @@ def get_subset(list, domain_list):
         if common_list:
             protein_subset.append(protein)
     return protein_subset
+
+
+def get_apoptotic_subset(df):
+    df_tomahaq = pd.read_excel('resources/apoptotic_tomahaq_targets.xlsx')
+    uids = df_tomahaq['Uniprot ID'].tolist()
+    uids.remove('Q96PG8, Q9BXH1')
+    uids.append('Q96PG8')
+    uids.append('Q9BHX1')
+    df_subset = df.loc[df.Protein_Id.isin(uids)]
+    return df_subset
+
+
+def get_membrane_subset(df):
+    df_membrane = pd.read_table('resources/processed__BrCa_Sets1_12_AtoD_EtoH_AtoD2_EtoH2_DrugsA_B_Tryps_140806_Quant.tsv')
+    uids = df_membrane['Uniprot-l'].tolist()
+    df_subset = df.loc[df.Protein_Id.isin(uids)]
+    return df_subset
 
 
 def get_uniprot_id(id, db):
