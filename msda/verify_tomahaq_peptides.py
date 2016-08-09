2901cbecfe28d9db346b78daab90ad87f5abb9c0
subkar <sub.kartik@gmail.com>
2016-08-09 17:50:12 -0400

    peptide sequence are compared to protein sequences to detect n-terminal and c -termnal petides, the preeceding amino acid of each peptide sequence is also extracted so as to improve the filtering of tryptic peptides, peptide sequences that end in KK or RR or RK are also scored

--- msda/verify_tomahaq_peptides.py
+++ msda/verify_tomahaq_peptides.py
@@ -1,20 +1,21 @@
 import re
 import pandas as pd
 import phosphosite_client as pc
+import requests
 
 
 def make_report(file):
     f = open(file).readlines()
-    peptide_list = [s.strip().split('\n')[0] for s in f]
-    df = prune_list(peptide_list)
-    df_ptm = check_ptm_redundancy(df)
     uid = file.strip().split('_')[0]
+    peptide_list = [s.strip().split('\n')[0] for s in f]
+    df = prune_list(peptide_list, uid)
+    df_ptm = check_ptm_redundancy(df)
     df_ptm.to_csv('%s_report.csv' % uid, index=False)
 
 
-def prune_list(peptide_list):
-    tryptic_petides = [p for p in peptide_list if _is_tryptic(p)]
-    l1, l2, l3, l4, l5 = [], [], [], [], []
+def prune_list(peptide_list, uid):
+    tryptic_petides = [p for p in peptide_list if _is_tryptic(p, uid)]
+    l1, l2, l3, l4, l5, l6 = [], [], [], [], [], []
     for pep in tryptic_petides:
         score = score_(pep)
         l1.append(score[0])
@@ -22,18 +23,21 @@ def prune_list(peptide_list):
         l3.append(score[2])
         l4.append(score[3])
         l5.append(score[4])
+        l6.append(score[5])
 
-    df = pd.DataFrame(zip(tryptic_petides, l1, l2, l3, l4, l5),
+    df = pd.DataFrame(zip(tryptic_petides, l1, l2, l3, l4, l5, l6),
                       columns=['sequence', 'starts_with_EorDorQ',
                                'starts_with_KKorRRorRK',
+                               'ends_with_KKorRRorRK',
                                'ends_with_KEorKDorRDorRE',
                                'length_crit', 'score'])
     return df
 
 
-def _is_tryptic(peptide_seq):
+def _is_tryptic(peptide_seq, uid):
     tr = not verify_cm(peptide_seq) and verify_kr_end(
-        peptide_seq) and not verify_kr_inner(peptide_seq)
+        peptide_seq, uid) and not verify_kr_inner(
+            peptide_seq) and verify_preceeding(peptide_seq, uid)
     return tr
 
 
@@ -43,10 +47,14 @@ def verify_cm(peptide_seq):
     return cm
 
 
-def verify_kr_end(peptide_seq):
-    kr_end = bool(re.search('K$', peptide_seq)) or bool(
-        re.search('R$', peptide_seq))
-    return kr_end
+def verify_kr_end(peptide_seq, uid):
+    if verify_cterminal(peptide_seq, uid):
+        end = True
+        return end
+    else:
+        kr_end = bool(re.search('K$', peptide_seq)) or bool(
+            re.search('R$', peptide_seq))
+        return kr_end
 
 
 def verify_kr_inner(peptide_seq):
@@ -55,8 +63,42 @@ def verify_kr_inner(peptide_seq):
     return kr_inner
 
 
+def verify_preceeding(peptide_seq, uid):
+    url = 'http://www.uniprot.org/uniprot/%s.fasta' % uid
+    r = requests.get(url)
+    fasta_output = str(r.text)
+    fasta_lines = fasta_output.strip().split('\n')
+    protein_sequence = ''.join(fasta_lines[1:])
+    start_ind = protein_sequence.find(peptide_seq)
+    if start_ind >= 1:
+        pa = protein_sequence[start_ind - 1]
+    elif start_ind == 0:
+        pa = 'n_terminal'
+    elif start_ind == -1:
+        pa = 'no match'
+    if (pa == 'M') or (pa == 'K') or (pa == 'R') or (pa == 'n_terminal'):
+        tryptic = True
+    else:
+        tryptic = False
+    return tryptic
+
+
+def verify_cterminal(peptide_seq, uid):
+    url = 'http://www.uniprot.org/uniprot/%s.fasta' % uid
+    r = requests.get(url)
+    fasta_output = str(r.text)
+    fasta_lines = fasta_output.strip().split('\n')
+    protein_sequence = ''.join(fasta_lines[1:])
+    start_ind = protein_sequence.find(peptide_seq)
+    if start_ind + len(peptide_seq) == len(protein_sequence):
+        c_terminal = True
+    else:
+        c_terminal = False
+    return c_terminal
+        
+
 def score_(peptide_seq):
-    edq, kr, ked, length_crit, score = False, False, False, False,  0
+    edq, kr, kre, ked, length_crit, score = False, False, False, False, False,  0
     if bool(re.search("^[E,D,Q]", peptide_seq)):
         edq = True
         score -= 1
@@ -65,6 +107,12 @@ def score_(peptide_seq):
                 re.search("^RK", peptide_seq)):
         kr = True
         score -= 1
+    if bool(re.search("KK$", peptide_seq)) or bool(
+            re.search("RR$", peptide_seq)) or bool(
+                re.search("RK$", peptide_seq)):
+        kre = True
+        score -= 1
+    
     if bool(re.search("KE$", peptide_seq)) or bool(
             re.search("KD$", peptide_seq)) or bool(
                 re.search("RD$", peptide_seq)) or bool(
@@ -74,7 +122,7 @@ def score_(peptide_seq):
     if not 5 <= len(peptide_seq) < 35:
         length_crit = True
         score -= 1
-    return edq, kr, ked, length_crit, score
+    return edq, kr, kre, ked, length_crit, score
 
 
 def check_ptm_redundancy(df):
@@ -90,27 +138,33 @@ def check_ptm_redundancy(df):
     df_ptm['query_sequence'] = [seq.upper()
                                 for seq in df_ptm.Sequence.tolist()]
     site_list = []
-    redundancy_list = []
+    redundancy_list_name = []
+    redundancy_list_id = []
 
     for seq in df.sequence.tolist():
-        red = df_ptm.Protein[df_ptm.query_sequence == seq].tolist()
-        redundancy_list.append([pr for pr in red if type(pr) == unicode])
+        red1 = df_ptm.Protein[df_ptm.query_sequence == seq].tolist()
+        red2 = df_ptm.Accession[df_ptm.query_sequence == seq].tolist()
+        redundancy_list_name.append([str(pr) for pr in red1
+                                     if type(pr) == unicode])
+        redundancy_list_id.append([str(pr).strip().split(':')[1] for pr in red2
+                                   if type(pr) == unicode])
         sites = df_ptm.Site[df_ptm.query_sequence == seq].tolist()
         site_list.append([s for s in sites if type(s) == unicode])
 
-    for i, l in enumerate(redundancy_list):
+    for i, l in enumerate(redundancy_list_id):
         if not l:
-            redundancy_list[i] = 'unknown'
-            site_list[i] = 'unknown'
+            redundancy_list_name[i] = 'unknown'
+            redundancy_list_id[i] = 'unknown'
+            # site_list[i] = 'unknown'
 
     for i, ptm in enumerate(site_list):
         if not ptm:
             site_list[i] = 'no PTM reported'
 
     df_update = df.copy()
-    df_update['redundancy'] = redundancy_list
+    df_update['matches_name'] = redundancy_list_name
+    df_update['matches_uid'] = redundancy_list_id
     df_update['PTM_sites'] = site_list
 
     return df_update
-        
 
