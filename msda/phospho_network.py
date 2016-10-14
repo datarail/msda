70d5052af6edff2c6fd0d75ca2a0e8d62759b769
subkar <sub.kartik@gmail.com>
2016-10-14 10:09:25 -0400

    networkin kinase annotations extracted, kinase gene set libraries set up based on extraction

--- msda/phospho_network.py
+++ msda/phospho_network.py
@@ -9,6 +9,7 @@ file = ('/Users/kartik/Dropbox (HMS-LSP)/BrCaLines_profiling/'
 df_ptm = pd.read_table('resources/Regulatory_sites_appended.tsv',
                        error_bad_lines=False)
 df_kinase = pd.read_csv('resources/kinase_substrate_dataset.csv')
+df_networkin = pd.read_table('resources/networkin_human_predictions.tsv')
 
 df1 = pd.read_excel(file, sheetname=0, header=1)
 df2 = pd.read_excel(file, sheetname=1, header=1)
@@ -20,7 +21,7 @@ def rename_columns(df):
     df = df.rename(columns={'Protein Id': 'Protein_ID',
                             'proteinID': 'Protein_ID',
                             'Site Position': 'Site_Position',
-                            'siteIDstr': 'Site_Position',
+                            # 'siteIDstr': 'Site_Position',
                             'geneSymbol': 'Gene_Symbol',
                             'gene_symbol': 'Gene_Symbol',
                             'motifPeptideStr': 'Motif'})
@@ -61,6 +62,12 @@ ptm_uids = df_ptm.ACC_ID.tolist()
 
 kinase_motifs = [m.upper()[1:-1] for m in df_kinase['SITE_+/-7_AA'].tolist()]
 kinase_uids = df_kinase.SUB_ACC_ID.tolist()
+
+nkin_motifs = [m.upper() for m in df_networkin.sequence.tolist()]
+kinase_motifs11 = [k[1:-1] for k in kinase_motifs]
+pMS_motifs = [k[1:-1] for k in motifs]
+df_input.Motif = pMS_motifs
+
 
 venn2([set(motifs), set(ptm_motifs)],
       set_labels=('tnbc_phospho', 'downstream_annotations'))
@@ -112,23 +119,37 @@ def get_kinases(motif, organism=None):
         kinase_id = 'nan'
         kin_org = 'nan'
         sub_org = 'nan'
-                                                                 
     return kinase, kinase_id, kin_org, sub_org
 
 
+def get_networkin_kinases(motif):
+    motifp = motif[:6] + motif[6].lower() + motif[7:]
+    df_motif = df_networkin[df_networkin.sequence == motifp[1:-1]]
+    if not df_motif.empty:
+        highest_score = df_motif.networkin_score.max()
+        highest_scoring_kinase = df_motif.id[
+            df_motif.networkin_score == highest_score].values[0]
+    else:
+        highest_scoring_kinase = 'nan'
+    return highest_scoring_kinase
+
+
 def generate_kinase_table(df_input):
-    df_input_kinase = df_input[df_input.Motif.isin(kinase_motifs)]
+    df_input_kinase = df_input[df_input.Motif.isin(all_motifs)]
     df_input_kinase = df_input_kinase.drop_duplicates()
     kinase_names, kinase_ids, orgs = [], [], []
+    networkin_kinases = []
     for motif in df_input_kinase.Motif.tolist():
         out = get_kinases(motif)
         kinase_names.append(out[0])
         kinase_ids.append(out[1])
         orgs.append("%s_%s" % (out[2], out[3]))
+        networkin_kinases.append(get_networkin_kinases(motif))
     df_output = df_input_kinase.copy()
     df_output['KINASE'] = kinase_names
     df_output['KINASE_ID'] = kinase_ids
     df_output['organisms'] = orgs
+    df_output['networkin_kinase'] = networkin_kinases
     return df_output
 
 df_out = generate_kinase_table(df_input)
@@ -151,4 +172,30 @@ def generate_network(df_output):
         G.add_node(kinase, UP=kinase_id)
         G.add_edge(kinase, substrate, site=site)
     return G
+
+
+def generate_ksea_library(kin_sub_table):
+    df = pd.read_csv(kin_sub_table)
+    psp_kinase = [k for k in df.KINASE.tolist() if k != float]
+    networkin_kinases = [k for k in df.networkin_kinase.tolist()
+                         if k != float]
+    all_kinases = list(set(psp_kinase + networkin_kinases))
+
+    gene_sets = []
+    for kinase in all_kinases:
+        df1 = df[df.KINASE == kinase]
+        subs = df1.Gene_Symbol.tolist()
+        sites = df1.Site.tolist()
+        sub_sites = ['%s_%s' % (sub, site) for sub, site
+                     in zip(subs, sites)]
+        df2 = df[df.networkin_kinase == kinase]
+        subs = df2.Gene_Symbol.tolist()
+        sites = df2.Site.tolist()
+        ss = ['%s_%s' % (sub, site) for sub, site in zip(subs, sites)]
+        sub_sites = list(set(sub_sites + ss))
+        if len(sub_sites) >= 25:
+            gene_set = [kinase, ' '] + sub_sites
+            gene_sets.append('\t'.join(gene_set))
+    return gene_sets        
+            
        
