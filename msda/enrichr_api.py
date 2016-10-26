ea077dc567934cd29531488a7da4d15c0c76585f
subkar <sub.kartik@gmail.com>
2016-10-26 17:58:34 -0400

    fix error in creating background files

--- msda/enrichr_api.py
+++ msda/enrichr_api.py
@@ -7,12 +7,14 @@ import shutil
 
 
 def prune_by_background(library, background):
+    bc = open(background).readlines()
+    bc_list = [pr.split('\n')[0] for pr in bc]
     terms = open('enrichr_libraries/%s.txt' % library).readlines()
     new_terms = []
     for term in terms:
         gene_list = term.split('\t')[2:-1]
         genes_in_background = [gene for gene in gene_list
-                               if gene in background]
+                               if gene in bc_list]
         new_term = '\t'.join(term.split('\t')[0: 2] +
                              genes_in_background + ['\n'])
         new_terms.append(new_term)
@@ -24,24 +26,19 @@ def get_enrichment(gene_list, library, background=None,
     output_xml = '%s.xml' % output_file
     if background:
         lib_list = prune_by_background(library, background)
-        with open('jars/%s.txt' %
-                  library, 'wb') as f:
-                f.write(lib_list)
-        lib = 'jars/%s.txt' % library
-        subprocess.call(['java', '-jar', 'jars/enrichr.jar',
-                         gene_list, lib, output_xml])
+        with open('enrichr_libraries_bc/%s.txt' % library, 'wb') as f:
+            for term in lib_list:
+                f.write('%s\n' % term)
+        lib = 'enrichr_libraries_bc/%s.txt' % library
     else:
-        shutil.copyfile('enrichr_libraries/%s.txt' % library,
-                        'jars/%s.txt' % library)
-        lib = 'jars/%s.txt' % library
-        print gene_list, library, output_xml
-        subprocess.call(['java', '-jar', 'jars/enrichr.jar',
-                         gene_list, lib, output_xml])
+        lib = 'enrichr_libraries/%s.txt' % library
+    subprocess.call(['java', '-jar', 'jars/enrichr.jar',
+                     gene_list, lib, output_xml])
     time.sleep(1)
     if os.path.isfile('%s_%s.tsv' % (output_file, library)):
         df = pd.read_table('%s_%s.tsv' % (output_file, library))
         df = get_adjusted_pvals(df)
-        os.remove(lib)
+        # os.remove(lib)
     else:
         df = None
     return df
