fad37f83a4613fcb4c5bba7f35ed20fb8120abe9
subkar <sub.kartik@gmail.com>
2016-11-30 12:30:13 -0500

    rename Protein_Id with Uniprot_Id, include try except for extracting core uniprot id from full identifier, included additional depricated uniprot id in ignore list

--- msda/preprocessing.py
+++ msda/preprocessing.py
@@ -4,7 +4,7 @@ from mapping import uid2gn
 
 df_map = pd.read_csv('resources/Uniprot_sec_to_prim.csv', sep='\t')
 
-delac_tr = ['C9JYP6']
+delac_tr = ['C9JYP6', 'Q7Z469']
 
 
 def get_samples(key_file):
@@ -29,40 +29,47 @@ def pd_import(file, sample_list=[]):
         df = pd.read_csv(file)
     else:
         print file, "is not a supported filetype"
-    columns = [re.sub(" ", "_", str(i)) for i in df.columns]
+    df = df.rename(columns={'Protein_Id': 'Uniprot_Id',
+                            'Protein Id': 'Uniprot_Id',
+                            'gene_symbol': 'Gene_Symbol'})
+    cols = [re.sub(" ", "_", str(i)) for i in df.columns]
 
     if sample_list:
-        sample_index = [columns.index(s) for s in columns
+        sample_index = [cols.index(s) for s in cols
                         if "_sn_" in s]
-        columns[sample_index[0]: sample_index[-1]+1] = sample_list
-    df.columns = columns
+        cols[sample_index[0]: sample_index[-1]+1] = sample_list
+    df.columns = cols
 
-    uniprot_id = [re.split('\|', i)[1] for i in df.Protein_Id]
-    df.Protein_Id = uniprot_id
+    df = df[~df.Uniprot_Id.str.contains('UPSP')]
+    try:
+        uniprot_id = [re.split('\|', i)[1] for i in df.Uniprot_Id.tolist()]
+        df.Uniprot_Id = uniprot_id
+    except IndexError:
+        pass
 
     # Find secondary accession numbers in dataframe
-    sec_ids = list(set(df.Protein_Id.tolist()).
+    sec_ids = list(set(df.Uniprot_Id.tolist()).
                    intersection(df_map.Secondary_ID.tolist()))
 
     # Replace secondary accesion numbers with primary ID's
-    for i, pid in enumerate(df.Protein_Id):
+    for i, pid in enumerate(df.Uniprot_Id):
         if pid in sec_ids:
-            df.Protein_Id.iloc[i] = get_primary_ids(pid)
+            df.Uniprot_Id.iloc[i] = get_primary_ids(pid)
 
     # Fix datetime entries in Gene names
     for i, gs in enumerate(df.Gene_Symbol):
         if isinstance(gs, pd.datetime):
-            df.Gene_Symbol.ix[i] = uid2gn(df.Protein_Id.ix[i])
+            df.Gene_Symbol.ix[i] = uid2gn(df.Uniprot_Id.ix[i])
 
     for i, gs in enumerate(df.Gene_Symbol):
         if type(gs) == float:
-            df.Gene_Symbol.ix[i] = uid2gn(df.Protein_Id.ix[i])
+            df.Gene_Symbol.ix[i] = uid2gn(df.Uniprot_Id.ix[i])
 
     # Remove deleted uniprot ids
-    df = df[~df.Protein_Id.isin(delac_tr)]
+    df = df[~df.Uniprot_Id.isin(delac_tr)]
 
-    df['Protein_Id_prefix'] = [pr.strip().split('-')[0]
-                               for pr in df.Protein_Id.tolist()]
+    df['Uniprot_Id_prefix'] = [pr.strip().split('-')[0]
+                               for pr in df.Uniprot_Id.tolist()]
 
     return df
 
