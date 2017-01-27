7c2bb53afd35dde460eb7aeee5c7e941bb5890bd
subkar <sub.kartik@gmail.com>
2017-01-27 14:41:10 -0500

    argument for limiting number of sets plotted included in plot_nes function

--- msda/gsea_tool.py
+++ msda/gsea_tool.py
@@ -30,21 +30,21 @@ def get_gsea_enrichment(input_file, library, out_folder=None, out_file=None):
     file1 = '%s/gsea_report_for_na_neg_%s.xls' % (terminal_path, timestamp)
     file2 = '%s/gsea_report_for_na_pos_%s.xls' % (terminal_path, timestamp)
     try:
-        df1 = pd.read_table(file1)
-        df2 = pd.read_table(file2)
+        df_pos = pd.read_table(file1)
+        df_neg = pd.read_table(file2)
     except IOError:
         pass
-    df = pd.concat([df1, df2])
+    df = pd.concat([df_pos, df_neg])
     return df
 
 
-def plot_nes(df, filter=False, outfile=None):
+def plot_nes(df, filter=False, top=5, outfile=None):
+    df1 = df[df.NES > 0].iloc[:top, :]
+    df2 = df[df.NES < 0].iloc[:top, :]
+    df = pd.concat([df1, df2])
     if filter is True:
         df = df[(df['NOM p-val'] <= 0.05) & (df['FDR q-val'] <= 0.25)]
-    # names = df.NAME.tolist()
-    # names2 = [s.split('_') for s in names]
-    # names3 = ['_'.join(s[1:]) for s in names2]
-    # df.NAME = names3
+    plt.ion()
     f, ax = plt.subplots()
     ax = sns.barplot(x='NES', y='NAME', data=df, color='b')
     ax.set(ylabel="", xlabel="Normalzied Enrichment Score (NES)")
@@ -52,7 +52,6 @@ def plot_nes(df, filter=False, outfile=None):
     score_pos = [s+0.1 if s > 0 else s-0.3 for s in df['NES'].tolist()]
     ax.set_xlim([min(score_pos), max(score_pos)])
     for p, pval, sc in zip(ax.patches, pvals, score_pos):
-        # width = p.get_x()
         ya = p.get_y()
         ax.text(sc, ya+0.6, pval)
     if outfile:
@@ -60,20 +59,6 @@ def plot_nes(df, filter=False, outfile=None):
     else:
         plt.show()
     plt.clf()
-    return df
-
-    # nes = df.NES.tolist()
-    # # names += df2.NAME.tolist()
-    # # names = '_'.join([n.split('_')[1:] for n in names])
-    # # nes += df2.NES.tolist()
-    # # pos = np.arange(len(nes)) + 0.5
-    # barh(pos, nes, align='center')
-    # yticks(pos, tuple(names))
-    # # lib = path.split('/')[-2].split('_')[-2]
-    # plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
-    # xlabel('NES')
-    # plt.savefig('nes1.png', dpi=600)
-    # plt.clf()
 
 
 def make_rnkfile(df, sample1, sample2):
@@ -82,6 +67,7 @@ def make_rnkfile(df, sample1, sample2):
     df2[sample1] = df2[sample1].apply(np.log2)
     df2 = df2.sort([sample1])
     df2 = df2[['Gene_Symbol', sample1]]
+    df2.Gene_Symbol = [g.upper() for g in df2.Gene_Symbol.tolist()]
     df2 = df2.replace([-np.inf, np.inf], np.nan)
     df2 = df2.dropna()
     return df2
