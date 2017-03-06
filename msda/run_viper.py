6e1061a74e3f161749682d3d0eeb639dd1232fa7
subkar <sub.kartik@gmail.com>
2017-03-06 17:05:23 -0500

    added optional argument that specifies column to be used as identifier

--- msda/run_viper.py
+++ msda/run_viper.py
@@ -1,17 +1,20 @@
 import subprocess
 
 
-def sum_duplicate_rows(df, filename='expr_temp.csv'):
-    df.Gene_Symbol = [g.upper() for g in df.Gene_Symbol.tolist()]
-    df2 = df.sort_values('Gene_Symbol').groupby('Gene_Symbol').mean()
-    df2.insert(0, 'Gene_Symbol', df2.index.tolist())
+def sum_duplicate_rows(df, identifier='Gene_Symbol',
+                       filename='expr_temp.csv'):
+    if identifier == 'Gene_Symbol':
+        df[identifier] = [g.upper() for g in df[identifier].tolist()]
+    df2 = df.sort_values(identifier).groupby(identifier).mean()
+    df2.insert(0, identifier, df2.index.tolist())
     df2.to_csv(filename, index=False)
     return df2
 
 
 def run_viper(df, type='ss', category=None, metafile=None, 
-              test=None, ref=None, outfile='viper_out.csv'):
-    df2 = sum_duplicate_rows(df)
+              test=None, ref=None, outfile='viper_out.csv',
+              identifier='Gene_Symbol'):
+    df2 = sum_duplicate_rows(df, identifier)
     if type == 'ss':
         subprocess.call(['Rscript', '--vanilla', 'viper/viper.R',
                          'expr_temp.csv', metafile, outfile])
