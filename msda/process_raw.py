627bea5e93980c730f0eb0767e004a16e1f7d0fd
subkar <sub.kartik@gmail.com>
2017-03-06 17:03:18 -0500

    colum names standardized, scaling function incuded

--- msda/process_raw.py
+++ msda/process_raw.py
@@ -2,19 +2,26 @@ import pandas as pd
 import os
 import numpy as np
 
-path = '../data/test_merge/'
-files = os.listdir(path)
+# path = '../data/test_merge/'
+# files = os.listdir(path)
 
-df_list = [pd.read_excel("%s%s" % (path, file))
-           for file in files]
+# df_list = [pd.read_excel("%s%s" % (path, file))
+#           for file in files]
 
 
 def remove_zeros(df):
-    df = df.rename(columns={'proteinID': 'Protein Id',
-                            'geneSymbol': 'gene_symbol'})
+    df = df.rename(columns={'proteinID': 'Uniprot_Id',
+                            'Protein Id': 'Uniprot_Id',
+                            'geneSymbol': 'Gene_Symbol',
+                            'gene_symbol': 'Gene_Symbol',
+                            'Site Position': 'Site_Position',
+                            'maxScoreStr': 'Max_Score',
+                            'motifPeptideStr': 'Motif',
+                            'Max Score': 'Max_Score',
+                            'sitePosStr': 'Site_Position'})
     samples = [c for c in df.columns.tolist() if '_sn_sum' in c]
     df2 = df[~(df[samples] == 0).all(axis=1)]
-    df3 = df2[~df2['Protein Id'].str.contains('HUMAN_contaminant')]
+    df3 = df2[~df2['Uniprot_Id'].str.contains('HUMAN_contaminant')]
     return df3
 
 
@@ -26,16 +33,24 @@ def merge(df_list):
 
     ref_samples = [c for c in ref_df.columns.tolist()
                    if '_sn_sum' in c]
-    nrm_factor = ref_df[samples].sum().max()
-    sample_sum = ref_df[samples].sum().values.tolist()
+    nrm_factor = ref_df[ref_samples].sum().max()
+    sample_sum = ref_df[ref_samples].sum().values.tolist()
     nr_list = [nrm_factor / float(s) for s in sample_sum]
     df_nrm_list = [normalize(d, nr_list) for d in df_list]
-    df_out = pd.marge([df_nrm_list], ignore_index=True)
+    df_out = pd.concat(df_nrm_list, ignore_index=True)
     return df_out
-    
-    
+
+
 def normalize(df, nrm_list):
-    samples =  [c for c in df.columns.tolist() if '_sn_sum' in c]
+    samples = [c for c in df.columns.tolist() if '_sn_sum' in c]
     df2 = df.copy()
     df2[samples] = df2[samples].multiply(nrm_list)
+    return df2
+
+
+def scale(df, samples):
+    df2 = df.copy()
+    df2[samples] = df2[samples].div(df2[samples].sum(axis=1),
+                                    axis=0)
+    df2[samples] = df2[samples].multiply(100)
     return df2
