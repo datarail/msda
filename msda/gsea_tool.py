ae09153689fb81b9fd688491e85d5aea66c60148
subkar <sub.kartik@gmail.com>
2017-03-06 17:01:25 -0500

    refactor plotting function for enrichment scores

--- msda/gsea_tool.py
+++ msda/gsea_tool.py
@@ -38,29 +38,6 @@ def get_gsea_enrichment(input_file, library, out_folder=None, out_file=None):
     return df
 
 
-def plot_nes(df, filter=False, top=5, outfile=None):
-    df1 = df[df.NES > 0].iloc[:top, :]
-    df2 = df[df.NES < 0].iloc[:top, :]
-    df = pd.concat([df1, df2])
-    if filter is True:
-        df = df[(df['NOM p-val'] <= 0.05) & (df['FDR q-val'] <= 0.25)]
-    plt.ion()
-    f, ax = plt.subplots()
-    ax = sns.barplot(x='NES', y='NAME', data=df, color='b')
-    ax.set(ylabel="", xlabel="Normalzied Enrichment Score (NES)")
-    pvals = ["%.3f" % p for p in df['NOM p-val'].tolist()]
-    score_pos = [s+0.1 if s > 0 else s-0.3 for s in df['NES'].tolist()]
-    ax.set_xlim([min(score_pos), max(score_pos)])
-    for p, pval, sc in zip(ax.patches, pvals, score_pos):
-        ya = p.get_y()
-        ax.text(sc, ya+0.6, pval)
-    if outfile:
-        plt.savefig(outfile, dpi=600)
-    else:
-        plt.show()
-    plt.clf()
-
-
 def make_rnkfile(df, sample1, sample2):
     df2 = df.copy()
     df2[sample1] = df2[sample1].div(df2[sample2])
@@ -72,4 +49,58 @@ def make_rnkfile(df, sample1, sample2):
     df2 = df2.dropna()
     return df2
 
+
+def plot_nes(df, filter=False, top=10, outfile=None,
+              fontsize=12, show_pval=False):
+    sns.set_style('dark')
+    font = {'family': 'sans-serif',
+            'color':  'black',
+            'weight': 'normal',
+            'size': fontsize,
+            }
+    sns.despine()
+    if filter is True:
+        df = df[(df['NOM p-val'] <= 0.05) & (df['FDR q-val'] <= 0.25)]
+    df1 = df[df.NES > 0].iloc[:top, :]
+    df2 = df[df.NES < 0].iloc[:top, :].sort_values(by='NES', ascending=False)
+    ylen = np.max([len(df1), len(df2)])
+    f, axes = plt.subplots(nrows=1, ncols=2)
+    if not df1.empty:
+        df1.NES.plot(ax=axes[1], kind='barh', width=0.3,
+                     color='Green', alpha=0.5)
+        for pos, name in zip(range(ylen), df1.NAME.tolist()):
+            axes[1].text(0, pos-0.25, name, horizontalalignment='left',
+                         fontdict=font)
+    if not df2.empty:
+        df2.NES.plot(ax=axes[0], kind='barh', width=0.3,
+                     color='Red', alpha=0.5)
+        for pos, name in zip(range(ylen), df2.NAME.tolist()):
+            axes[0].text(0, pos+0.25, name, horizontalalignment='right',
+                         fontdict=font)
+    axes[0].set_yticks([])
+    axes[1].set_yticks([])
+    plt.subplots_adjust(wspace=0.075)
+    axes[0].set_ylim([-0.5, ylen-0.5])
+    axes[1].set_ylim([-0.5, ylen-0.5])
+
+    if show_pval:
+        pvals1 = ["%.2f" % p if p > 0 else "%.1f" % p
+                  for p in df1['NOM p-val'].tolist()]
+        pvals2 = ["%.2f" % p if p > 0 else "%.1f" % p
+                  for p in df2['NOM p-val'].tolist()] 
+        for x_pos, y_pos, pval in zip(df1.NES.tolist(), range(ylen),
+                                      pvals1):
+            axes[1].text(x_pos+0.1, y_pos, pval)
+
+        for x_pos, y_pos, pval in zip(df2.NES.tolist(), range(ylen)[::-1],
+                                      pvals2):
+            axes[1].text(x_pos-0.5, y_pos, pval)
+    plt.gca().invert_yaxis()
+    if not outfile:
+        plt.show()
+        plt.clf()
+    else:
+        plt.savefig(outfile)
+        plt.clf()
+        
 
