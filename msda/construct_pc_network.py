e1a8e33b88a69d3e45c5a12aa28b666811551201
subkar <sub.kartik@gmail.com>
2017-03-10 12:21:04 -0500

    script to construct and plot pathway commons network for a given list of genes

---
+++ msda/construct_pc_network.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+import subprocess
+
+
+def get_pathsbetween(source_list, filter=False):
+    base_url = 'http://www.pathwaycommons.org/pc2/graph'
+    source_str = ','.join(source_list)
+    params = {'source': source_str,
+              'kind': 'PATHSBETWEEN',
+              'format': 'EXTENDED_BINARY_SIF'}
+    r = requests.get(base_url, params=params)
+    with open('temp_pc_interactions.txt', 'wb') as f:
+        f.write(r.text)
+    df = pd.read_table('temp_pc_interactions.txt')
+    if filter:
+        df = df[(df.PARTICIPANT_A.isin(source_list)) & (
+            df.PARTICIPANT_B.isin(source_list))]
+    return df
+
+
+def make_network_plot(weights_file, outfile):
+    df = pd.read_csv(weights_file)
+    source_list = df['0'].tolist()
+    df = get_pathsbetween(source_list, filter=True)
+    df2 = df[df.columns[:3]]
+    df2.to_csv('temp_pc_file.txt', sep='\t', index=False)
+    subprocess.call(['Rscript', 'netcontext.R', '-n', 'temp_pc_file.txt',
+                     '-w',  weights_file, '-o', outfile])
+    return df2
+
