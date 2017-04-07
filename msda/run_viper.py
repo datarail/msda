495a8f56c3796d771022b30ef82e5363b7890e64
subkar <sub.kartik@gmail.com>
2017-04-07 13:57:22 -0400

    set absolute path for R script and regulon files, pass regulon as an argument to run_viper function

--- msda/run_viper.py
+++ msda/run_viper.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 
 def sum_duplicate_rows(df, identifier='Gene_Symbol',
@@ -13,16 +14,18 @@ def sum_duplicate_rows(df, identifier='Gene_Symbol',
 
 def run_viper(df, type='ss', category=None, metafile=None, 
               test=None, ref=None, outfile='viper_out.csv',
-              identifier='Gene_Symbol'):
+              identifier='Gene_Symbol', regulon='brca-tf-regulon.rda'):
     df2 = sum_duplicate_rows(df, identifier)
+    viper_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viper')
+    regulon_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viper/regulons/%s' % regulon)
     if type == 'ss':
-        subprocess.call(['Rscript', '--vanilla', 'viper/viper.R',
+        subprocess.call(['Rscript', '--vanilla', os.path.join(viper_path, 'viper.R'),
                          'expr_temp.csv', metafile, outfile])
     elif type == 'ms':
-        subprocess.call(['Rscript', '--vanilla', 'viper/msviper.R',
+        subprocess.call(['Rscript', '--vanilla', os.path.join(viper_path, 'msviper.R'),
                          'expr_temp.csv', metafile, category,
                          test, ref, outfile])
     elif type == 'fc':
-        subprocess.call(['Rscript', '--vanilla', 'viper/fc_viper.R',
-                         'expr_temp.csv', outfile])
+        subprocess.call(['Rscript', '--vanilla', os.path.join(viper_path, 'fc_viper.R'),
+                         'expr_temp.csv', regulon_path, outfile])
 
