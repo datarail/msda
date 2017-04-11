bd9b22109cdf5ccfe9a41f165b339c7d189a2068
subkar <sub.kartik@gmail.com>
2017-04-11 13:34:55 -0400

    add missing network style definition

--- msda/construct_pc_network.py
+++ msda/construct_pc_network.py
@@ -9,7 +9,6 @@ import py2cytoscape.util.cytoscapejs as cyjs
 import py2cytoscape.cytoscapejs as renderer
 
 
-
 def get_pathsbetween(source_list, filter=False):
     """ Pahway commons client that retuens interactinos between
     genes in extended binary SIF format
@@ -88,36 +87,37 @@ def make_cytoscpe_plot(df_sif, weights_file):
     df_weights.columns = ['Name', 'Weight']
     df_weights.index = df_weights.Name.tolist()
 
-     
+    cy = CyRestClient()
+    cy.session.delete()
+    cynet = cy.network.create_from_networkx(G)
+    network_style = cy.style.create('GAL style')
+    cy.layout.apply(network=cynet)
+    cy.style.apply(network_style, network=cynet)
+    cynet.update_node_table(df=df_weights, network_key_col='name')
+
     basic_settings = {
-        # Node parameters.
+
+        # You can set default values as key-value pairs.
+
         'NODE_FILL_COLOR': '#6AACB8',
         'NODE_SIZE': 20,
         'NODE_BORDER_WIDTH': 0,
         'NODE_TRANSPARENCY': 120,
         'NODE_LABEL_COLOR': 'black',
-        # Edge parameters
+
         'EDGE_WIDTH': 2,
         'EDGE_TRANSPARENCY': 100,
         'EDGE_STROKE_UNSELECTED_PAINT': 'red',
         'EDGE_TARGET_ARROW_SHAPE': 'ARROW',
         'EDGE_TARGET_ARROW_UNSELECTED_PAINT': 'red',
-        # canvas parameters
+
         'NETWORK_BACKGROUND_PAINT': 'white'
     }
 
-    cy = CyRestClient()
-    cy.session.delete()  
-    
-    cynet = cy.network.create_from_networkx(G)
-    cy.layout.apply(network=cynet)   
-    cy.style.apply(network_style, network=cynet)
-    cynet.update_node_table(df=df_weights, network_key_col='name')
-    
     network_style.update_defaults(basic_settings)
     network_style.create_passthrough_mapping(
         column='name', vp='NODE_LABEL', col_type='String')
-
+    weights = cynet.get_node_column('Weight')
     color_gradients = StyleUtil.create_2_color_gradient(min=weights.min(),
                                                         max=weights.max(),
                                                         colors=('blue', 'red'))
@@ -127,25 +127,23 @@ def make_cytoscpe_plot(df_sif, weights_file):
                                             col_type='Double',
                                             points=color_gradients)
 
-    
-    edge_color_map = {'controls-state-change-of' : 'orange',
-                      'controls-phosphorylation-of': 'green' ,
-                      'controls-transport-of': 'yellow' ,
-                      'in-complex-with': 'blue' ,
+    edge_color_map = {'controls-state-change-of': 'orange',
+                      'controls-phosphorylation-of': 'green',
+                      'controls-transport-of': 'yellow',
+                      'in-complex-with': 'blue',
                       'controls-expression-of': 'red'}
 
-    edge_shape_map1 = {'controls-state-change-of' : 'ARROW',
-                      'controls-phosphorylation-of': 'ARROW' ,
-                      'controls-transport-of': 'ARROW' ,
-                      'in-complex-with': 'CIRCLE' ,
-                      'controls-expression-of': 'ARROW'}
+    edge_shape_map1 = {'controls-state-change-of': 'ARROW',
+                       'controls-phosphorylation-of': 'ARROW',
+                       'controls-transport-of': 'ARROW',
+                       'in-complex-with': 'CIRCLE',
+                       'controls-expression-of': 'ARROW'}
 
-    edge_shape_map2 = {'controls-state-change-of' : 'NONE',
-                      'controls-phosphorylation-of': 'NONE' ,
-                      'controls-transport-of': 'NONE' ,
-                      'in-complex-with': 'CIRCLE' ,
-                      'controls-expression-of': 'NONE'}
-
+    edge_shape_map2 = {'controls-state-change-of': 'NONE',
+                       'controls-phosphorylation-of': 'NONE',
+                       'controls-transport-of': 'NONE',
+                       'in-complex-with': 'CIRCLE',
+                       'controls-expression-of': 'NONE'}
 
     network_style.create_discrete_mapping(column='INTERACTION_TYPE',
                                           col_type='String',
@@ -172,5 +170,8 @@ def make_cytoscpe_plot(df_sif, weights_file):
                                           vp='EDGE_SOURCE_ARROW_SHAPE',
                                           mappings=edge_shape_map2)
 
+
+
+    
 
 
