225c1e82f95ac244f8086dd8fc2d595fc97769b5
subkar <sub.kartik@gmail.com>
2017-05-19 13:05:34 -0400

    initial commit of r script that takes kinase_substrate relationship csv file and outputs regulon object

---
+++ msda/viper/get_regulon.r
@@ -0,0 +1,14 @@
+get_regulon <- function(regul_file){
+  regul_mat <- read.delim(regul_file,sep = ',',as.is = T)
+  regulon <- list()
+  for(i in unique(regul_mat$KINASE)){
+    regul_mat_i <- regul_mat[regul_mat$KINASE == i,]
+    regulon[[i]] <- list('tfmode' = rep(1,nrow(regul_mat_i)),'likelihood'=regul_mat_i$confidence)
+    names(regulon[[i]]$likelihood) <- paste(regul_mat_i$Gene_Symbol, regul_mat_i$Site, sep = '_')
+  }
+  return(regulon)
+}
+
+regulon_file <- args[1]
+regulon <- get_regulon(regulon_file)
+save(regulon, file = args[2])
