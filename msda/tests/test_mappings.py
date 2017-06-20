25166ff743d20d702deecd591eda34505c0d16fd
subkar <sub.kartik@gmail.com>
2017-06-20 15:56:49 -0400

    additional tests for mapping fuctions

--- msda/tests/test_mappings.py
+++ msda/tests/test_mappings.py
@@ -10,6 +10,18 @@ def test_uid2gn():
     assert gene_name == 'CDK2'
 
 
+def test_name2entrez():
+    entrez_id = mapping.name2entrez('CDK2')
+    assert entrez_id == 1017
 
-    
+
+def test_entrez2name():
+    gene_name = mapping.entrez2name(1017)
+    assert gene_name == 'CDK2'
+
+
+
+def test_get_uniprot_id():
+    uid = mapping.get_uniprot_id('CDK2')
+    assert uid == 'P24941'
     
