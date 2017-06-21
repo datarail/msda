1d5d94c91a256a5140c598ea66ce2c1e2c258a70
subkar <sub.kartik@gmail.com>
2017-06-21 17:37:43 -0400

    function names in tests modified to match changes in mapping.py

--- msda/tests/test_mappings.py
+++ msda/tests/test_mappings.py
@@ -1,26 +1,26 @@
 from msda import mapping
 
-def test_ensp2uid():
-    id = mapping.ensp2uid('ENSP00000266970')
+def test_get_uniprot_from_ensembl():
+    id = mapping.get_uniprot_from_ensembl('ENSP00000266970')
     assert id == 'P24941'
 
 
-def test_uid2gn():
-    gene_name = mapping.uid2gn('P24941')
+def test_get_name_from_uniprot():
+    gene_name = mapping.get_name_from_uniprot('P24941')
     assert gene_name == 'CDK2'
 
 
-def test_name2entrez():
-    entrez_id = mapping.name2entrez('CDK2')
+def test_get_entrez_from_name():
+    entrez_id = mapping.get_entrez_from_name('CDK2')
     assert entrez_id == 1017
 
 
-def test_entrez2name():
-    gene_name = mapping.entrez2name(1017)
+def test_get_name_from_entrez():
+    gene_name = mapping.get_name_from_entrez(1017)
     assert gene_name == 'CDK2'
 
 
-def test_get_uniprot_id():
-    uid = mapping.get_uniprot_id('CDK2')
+def test_get_uniprot_from_name():
+    uid = mapping.get_uniprot_from_name('CDK2')
     assert uid == 'P24941'
     
