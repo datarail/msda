eb032ff0377b517d627ff9a541f4ba1e31eac18d
subkar <sub.kartik@gmail.com>
2017-06-21 16:02:49 -0400

    included functons to test for filtering reverse and human contaminant proteins

--- msda/tests/test_preprocessing.py
+++ msda/tests/test_preprocessing.py
@@ -14,18 +14,53 @@ def test_quantile_normalize():
                             'C2': {'A': 4.666667, 'B': 2,
                                    'C': 4.666667, 'D': 3.0},
                             'C3': {'A': 2.0, 'B': 3.0,
-                                   'C': 4.666667, 'D': 5.666667}})
-    
+                                   'C': 4.666667, 'D': 5.666667}})    
     assert_frame_equal(dfq, dfq_ref)
+
+
+def test_remove_human_contaminants():
+     df = pd.DataFrame({'Uniprot_Id': ['P1234', '2_HUMAN_contaminant',
+                                       'Q4567', '4_HUMAN_contaminant'],
+                        'C2': [4,  1, 4, 2],
+                        'C3': [3, 5, 6, 8]},
+                       index=['A', 'B',
+                              'C', 'D'])
+     
+     dfs = preprocessing.remove_human_contaminants(df)
+     dfs_ref = pd.DataFrame({'Uniprot_Id': ['P1234',  'Q4567'],
+                        'C2': [4, 4],
+                        'C3': [3, 6]},
+                       index=['A', 'C'])
+     
+     assert_frame_equal(dfs, dfs_ref)
+
+
+def test_remove_reverse_proteins():
+     df = pd.DataFrame({'Uniprot_Id': ['P1234', '##P1234',
+                                       'Q4567', '##Q4567'],
+                        'C2': [4,  1, 4, 2],
+                        'C3': [3, 5, 6, 8]},
+                       index=['A', 'B',
+                              'C', 'D'])
+     
+     dfs = preprocessing.remove_human_contaminants(df)
+     dfs_ref = pd.DataFrame({'Uniprot_Id': ['P1234',  'Q4567'],
+                        'C2': [4, 4],
+                        'C3': [3, 6]},
+                       index=['A', 'C'])
+     
+     assert_frame_equal(dfs, dfs_ref)
+    
+    
     
 
-def test_filter_score():
-    df = pd.DataFrame({'Max_Score': ['4;7', '15;2']},
-                      index=['BRAF', 'RAF1'])
-    dfs = preprocessing.filter_score(df)
-    dfs_ref =  pd.DataFrame({'Max_Score': ['15;2']},
-                      index=['RAF1'])
-    assert_frame_equal(dfs, dfs_ref)
+# def test_filter_score():
+#     df = pd.DataFrame({'Max_Score': ['4;7', '15;2']},
+#                       index=['BRAF', 'RAF1'])
+#     dfs = preprocessing.filter_score(df)
+#     dfs_ref =  pd.DataFrame({'Max_Score': ['15;2']},
+#                       index=['RAF1'])
+#     assert_frame_equal(dfs, dfs_ref)
 
 
 def test_get_primary_ids():
