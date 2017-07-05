f97a0bff33f56fa3807c5cd46eb064b5b8f235af
subkar <sub.kartik@gmail.com>
2017-07-05 11:24:18 -0400

    test and reference sample specifications passed as argument

--- msda/viper/msviper.R
+++ msda/viper/msviper.R
@@ -1,36 +1,28 @@
 library("Biobase")
 library("viper")
+library(stringr)
 args <- commandArgs(trailingOnly=TRUE)
 
 exprsFile <- args[1]
 identifier <- args[6]
 exprs <- as.matrix(read.table(exprsFile, header=TRUE, sep=',', row.names=identifier, as.is=TRUE))
 
-
 pDataFile <- args[2]
 pData <- read.table(pDataFile, row.names='Sample', header=TRUE, sep=',')
-
 all(rownames(pData)==colnames(exprs))
-
 phenoData <- new("AnnotatedDataFrame", data=pData)
 
 exSet <- ExpressionSet(assayData=exprs, phenoData=phenoData)
 
-exSet
-
-#signature <- rowTtest(exSet, args[3] , args[4], args[5])
-
-signature <- rowTtest(exSet, "Molecular_subtype", c("Basal", "Basal A", "Basal B"), "Luminal")
+test_str <- args[4]
+test_samples <- unlist(str_split(test_str, ","))
+ref_str <- args[5]
+ref_samples <- unlist(str_split(ref_str, ", "))
+signature <- rowTtest(exSet, args[3], test_samples, ref_samples)
 
 signature <- (qnorm(signature$p.value/2, lower.tail=FALSE) * sign(signature$statistic))[, 1]
+# nullmodel <- ttestNull(exSet, args[3], test_samples, ref_samples, per=1000, repos=TRUE, verbose=FALSE)
 
-# nullmodel <- ttestNull(exSet, "Molecular_subtype", c("Basal", "Basal A", "Basal B"), "Luminal", per=1000, repos=TRUE, verbose=FALSE)
-
-# nullmodel <- ttestNull(exSet, args[3], args[4], args[5], per=1000, repos=TRUE, verbose=FALSE)
-
-
-# chris_regulon <- load('viper/regulon_symbol.rdata')
-# brca_regulon <- load('viper/regulons/regul_symbol.rda')
 regulon <- load(args[7])
 
 mrs <- msviper(signature, get(regulon),  verbose=FALSE)
