1f37b613dceaf9f8617437f7a965b31695142f7b
subkar <sub.kartik@gmail.com>
2017-07-29 08:13:44 -0400

    renamed function 'remove zeros' to filter_contaminants, convert site position to string and remove reverse proteins

--- msda/process_raw.py
+++ msda/process_raw.py
@@ -9,7 +9,7 @@ import numpy as np
 #           for file in files]
 
 
-def remove_zeros(df):
+def filter_contaminants_reverse(df):
     df = df.rename(columns={'proteinID': 'Uniprot_Id',
                             'Protein Id': 'Uniprot_Id',
                             'geneSymbol': 'Gene_Symbol',
@@ -22,11 +22,13 @@ def remove_zeros(df):
     samples = [c for c in df.columns.tolist() if '_sn_sum' in c]
     df2 = df[~(df[samples] == 0).all(axis=1)]
     df3 = df2[~df2['Uniprot_Id'].str.contains('HUMAN_contaminant')]
-    return df3
+    df4 = df3[~df3['Uniprot_Id'].str.contains('##')]
+    df4['Site_Position'] = [str(sp) for sp in df4['Site_Position'].tolist()]
+    return df4
 
 
 def merge(df_list):
-    df_list = [remove_zeros(d) for d in df_list]
+    df_list = [filter_contaminants_reverse(d) for d in df_list]
     len_df = [len(d) for d in df_list]
     ref_index = len_df.index(max(len_df))
     ref_df = df_list[ref_index]
