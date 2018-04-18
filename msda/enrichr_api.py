4f4cbbc2bc4a28e4433c042807372c461f455c85
subkar <sub.kartik@gmail.com>
2018-04-18 16:49:39 -0400

    include try except cluase for getting FDR vals

--- msda/enrichr_api.py
+++ msda/enrichr_api.py
@@ -45,13 +45,13 @@ def get_enrichment(gene_list, library, background=None,
     msda_path = os.path.dirname(os.path.abspath(__file__))
     enrichr_input_path = os.path.join(msda_path, 'enrichr_input.txt')
     
-    with open(enrichr_input_path, 'wb') as f:
+    with open(enrichr_input_path, 'w') as f:
         for gene in gene_list:
             f.write("%s\n" % gene)
     if background:
         lib_list = prune_by_background(library, background)
         lib_path = os.path.join(msda_path, 'enrichr_libraries_bc')
-        with open('%s/%s.txt' % (lib_path, library), 'wb') as f:
+        with open('%s/%s.txt' % (lib_path, library), 'w') as f:
             for term in lib_list:
                 f.write('%s\n' % term)        
     else:
@@ -59,10 +59,13 @@ def get_enrichment(gene_list, library, background=None,
     lib = '%s/%s.txt' % (lib_path, library)    
     subprocess.call(['java', '-jar', os.path.join(msda_path, 'jars/enrichr.jar'),
                      enrichr_input_path, lib, output_xml])
-    time.sleep(10)    
+    time.sleep(2)    
     if os.path.isfile('%s_%s.tsv' % (output_file, library)):
         df = pd.read_table('%s_%s.tsv' % (output_file, library))
-        df = get_adjusted_pvals(df)        
+        try:
+            df = get_adjusted_pvals(df)
+        except TypeError:
+            pass
         # os.remove(lib)
     else:
         df = None
