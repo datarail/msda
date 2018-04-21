868fccb48fdd2a5e2227d6cfbc02ff5ec2e5beb4
subkar <sub.kartik@gmail.com>
2018-04-20 20:46:16 -0400

    path to output folder is made a mandatory argument instead of optional. Docstrings included for all functions

--- msda/gsea_tool.py
+++ msda/gsea_tool.py
@@ -1,27 +1,48 @@
 import subprocess
 import os
 import matplotlib.pyplot as plt
-from pylab import barh
 import numpy as np
 import pandas as pd
-from pylab import *
 import seaborn as sns
 
 
-def get_gsea_enrichment(input_file, library, out_folder=None, out_file=None):
+def get_gsea_enrichment(input_file, library, out_folder):
+    """Runs GSEA based on input set of genes with associated logFC values
+    using local jar file and gene set library (.gmt file)
+
+    Parameters
+    ----------
+    input_file : str
+        path to 2-column tab-seperated file  of gene (or phospshosite)
+        and associated log2 fold-change values. Extension should be .rnk
+    library : str
+        path to gene set library
+    out_folder : str
+        path to output folder where all output files will be saved
+
+    Returns
+    -------
+    df : pandas dataframe
+       Summary of results from enrichment analysis that includes
+       the term enriched, enrichment value, p-value and FDR.
+    """
     path = out_folder
     os.makedirs(path)
-    lib = '%s.gmt' % library
+    if '.gmt' not in library:
+        lib = '%s.gmt' % library
+    else:
+        lib = library
     msda_path = os.path.dirname(os.path.abspath(__file__))
     arg_list = ['java', '-cp', os.path.join(msda_path, 'jars/gsea2-2.2.3.jar'),
                 '-Xmx1024m',  'xtools.gsea.GseaPreranked',
                 '-param_file', os.path.join(msda_path, 'gsea_parameters.txt'),
                 '-rnk', input_file,
-                '-gmx', lib]
-    if out_folder:
-        arg_list += ['-out', path]
-    if out_file:
-        arg_list += ['-rpt_label', out_file]
+                '-gmx', lib,
+                '-out', path]
+    # if out_folder:
+    #     arg_list += ['-out', path]
+    # if out_file:
+    #     arg_list += ['-rpt_label', out_file]
     subprocess.call(arg_list)
     terminal_folder = os.listdir(path)[0]
     timestamp = terminal_folder.split('.')[2]
@@ -39,6 +60,26 @@ def get_gsea_enrichment(input_file, library, out_folder=None, out_file=None):
 
 
 def make_rnkfile(df, sample1, sample2, identifier):
+    """ Generated 2-column dataframe of identifier (gene or phosphosite)
+    and associated log2 fold-change values
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        proteomics or phosphoproteomics dataframe of dataset.
+    sample1 : str
+        column name of test sample.
+    sample2 : str
+        column name of control sample.
+    identifier : str
+       name of column that contains identifiers.
+
+    Results
+    -------
+    df2 : pandas dataframe
+       2-column dataframe of identifier (gene or phosphosite)
+       and associated log2 fold-change values
+    """
     df2 = df.copy()
     df2[sample1] = df2[sample1].div(df2[sample2])
     df2[sample1] = df2[sample1].apply(np.log2)
@@ -50,10 +91,37 @@ def make_rnkfile(df, sample1, sample2, identifier):
     return df2
 
 
-def plot_nes(df, filter=False, top=10, outfile=None,
+def plot_nes(df, filter=True, top=10, outfile=None,
              pval_label='NOM p-val', fdr_label='FDR q-val',
              regulon='NAME',
              fontsize=12, show_pval=False):
+    """Plots gene set terms and associated enrichment scores
+       from enrichment analyses.
+
+    Parameters
+    ----------
+    df : pandas dataframe
+       output dataframe from running enrichment analysis.
+       Should contain term name, enrichment score, associated p-value and FDR.
+    filter : Optional[bool]
+       If True, terms for which the p-value is less than 0.05
+       and FDR is less than 0.2 are filtered out. Default is True.
+    top : Optional[int]
+       Number of terms from downregulated and upregulated terms
+       to be included in the plot. Default is 10.
+    outfile : Optional[str]
+       name of file to save the plot. Default is None
+    pval_label : Optional[str]
+       Name of column that contains p-values. Default is 'NOM p-val'
+    fdr_label : Optional[str]
+       Name of column that contains FDR values. Default is 'FDR q-val'
+    regulon : Optional[str]
+       Name of column that contains name of terms. Default is 'NAME'.
+    fontsize : Optional[int]
+       Size of fonts in plot. Default is 10.
+    show_pval : bool
+       p-values shown on plot if True. Default is False.
+    """
     sns.set_style('dark')
     font = {'family': 'sans-serif',
             'color':  'black',
